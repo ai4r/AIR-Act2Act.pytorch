7c49079acafbc11f252964cef6261db49066bc48
Woo-Ri Ko <wrko@etri.re.kr>
2020-03-31 13:57:53 +0900

    Update recog_subaction.py

--- recog_subaction.py
+++ recog_subaction.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
-from sklearn.cluster import KMeans
 from tqdm import tqdm
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from scipy.spatial.distance import cdist
 
 from utils.AIR import norm_to_torso, denorm_from_torso
 from utils.draw import draw
@@ -31,8 +33,7 @@ def draw_plots(km_values):
         plt.show()
 
 
-# 학습할 수 있는 DataFrame 형식으로 만들기
-def make_dataframe(inputs, input_length):
+def make_null_dataframe(input_length):
     # feature 이름 생성
     # fAd == 해당 시퀀스의 frame A번째의 사람과 로봇사이 distance
     # fAjB == 해당 시퀀스의 frame A번째의 B번째 joint
@@ -40,7 +41,12 @@ def make_dataframe(inputs, input_length):
     feature_name.extend(
         [F"f{a}j{b+1}" for a in range(input_length) for b in range(24)])  # f0j1 ~ f0j24, f1j1 ~ f1j24, ...
 
-    df = pd.DataFrame(columns=feature_name)
+    return pd.DataFrame(columns=feature_name)
+
+
+# 학습할 수 있는 DataFrame 형식으로 만들기
+def make_dataframe(inputs, input_length):
+    df = make_null_dataframe(input_length)
     # pbar = tqdm(total=len(inputs))
     for i, a in enumerate(inputs):  # 파일들에 대해
         temp = dict()
@@ -99,29 +105,33 @@ def check_null(df):
     return col_list
 
 
-def train(action):
-    index = int(action[-2:]) - 1
+def train(actions, n_clusters):
+    # 모든 action에 대해 테이터를 모으기
+    train, test = make_null_dataframe(SEQ_LENGTH), make_null_dataframe(SEQ_LENGTH)
+    for action in actions:
+        print(F"\nAction: {action}")
+        train_files = glob.glob(os.path.join(train_path, f"*{action}*.npz"))
+        print(f'Train data loading. Total {len(train_files)} files.')
+        test_files = glob.glob(os.path.join(test_path, f"*{action}*.npz"))
+        print(f'Test data loading. Total {len(test_files)} files.')
 
-    print(F"\n\nAction: {action}")
-    train_files = glob.glob(os.path.join(train_path, f"*{action}*.npz"))
-    print(f'Train data loading. Total {len(train_files)} files.')
-    test_files = glob.glob(os.path.join(test_path, f"*{action}*.npz"))
-    print(f'Test data loading. Total {len(test_files)} files.')
-
-    print('Preprocessing...')
-    train = preprocessing(SEQ_LENGTH, train_files)
-    test = preprocessing(SEQ_LENGTH, test_files)
+        print('\nPreprocessing...')
+        train = train.append(preprocessing(SEQ_LENGTH, train_files), ignore_index=True, sort=False)
+        test = test.append(preprocessing(SEQ_LENGTH, test_files), ignore_index=True, sort=False)
 
     # 전체 데이터를 보기 위해서 train, test set 합침
     train = pd.concat([train, test], axis=0)
+
+    # N_CLUSTERS를 얼마로 하면 될지 찾기
+    # get_kmeans_insight(train)
 
     # null값 있는지 체크
     #     if( len(check_null(train)) == 0 and check_null(test) == 0 ):
     #         print("null value existed")
     #         break
 
-    print('K-means clustering...')
-    km = KMeans(n_clusters=N_CLUSTERS[index], random_state=2020)
+    print('\nK-means clustering...')
+    km = KMeans(n_clusters=n_clusters, random_state=2020)
     km.fit(train)
 
     # 결과값 그래프 그리기
@@ -134,23 +144,48 @@ def train(action):
     # 학습한 model save
     if not os.path.exists(model_path):
         os.makedirs(model_path)
-    with open(F"{model_path}/{action}_full_{N_CLUSTERS[index]}_cluster.pkl", "wb") as f:
+    with open(F"{model_path}/{''.join(actions)}_full_{n_clusters}_cluster.pkl", "wb") as f:
         pickle.dump(km, f)
     print('Model saved.')
 
 
+def get_kmeans_insight(df):
+    plt.rcParams["figure.figsize"] = (10, 10)
+    plt.rcParams['lines.linewidth'] = 10
+    plt.rcParams['lines.color'] = 'r'
+    plt.rcParams['axes.grid'] = True
+    distortions = []
+    ks = range(10, 20)
+    for k in ks:
+        km_model = KMeans(n_clusters=k, random_state=2020).fit(df)
+        distortions.append(sum(np.min(cdist(df, km_model.cluster_centers_, 'euclidean'), axis=1)) / df.shape[0])
+        cluster_labels = km_model.labels_
+        if k != 1:
+            # silhouette score
+            silhouette_avg = metrics.silhouette_score(df, cluster_labels)
+            print("For n_clusters={0}, the silhouette score is {1}".format(k, silhouette_avg))
+    # Plot the elbow
+    plt.plot(ks, distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Result')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
+
+
 def test():
     # 보고싶은 액션 입력
-    action = "A005"
-    index = int(action[-2:]) - 1
+    actions = ["A001", "A004", "A005", "A006", "A008"]
+    n_clusters = 15
 
-    model_file = os.path.join(model_path, f"{action}_full_{N_CLUSTERS[index]}_cluster.pkl")
+    model_file = os.path.join(model_path, f"{''.join(actions)}_full_{n_clusters}_cluster.pkl")
     if not os.path.exists(model_file):
-        train(action)
+        train(actions, n_clusters)
     km_model = pickle.load(open(model_file, "rb"))
 
     # show all test data
-    data_files = glob.glob(os.path.normpath(os.path.join(test_path, F"*{action}*.npz")))
+    data_files = list()
+    for action in actions:
+        data_files.extend(glob.glob(os.path.join(test_path, F"*{action}*.npz")))
     data_files.sort()
     n_data = len(data_files)
 
